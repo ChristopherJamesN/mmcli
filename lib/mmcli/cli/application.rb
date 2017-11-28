71d533b04c270a4f8f0acc47df2faea7496df022
ChristopherJamesN <nady.christopher@gmail.com>
2017-11-28 14:08:09 -0600

    Inlcude thor actions

--- lib/mmcli/cli/application.rb
+++ lib/mmcli/cli/application.rb
@@ -3,28 +3,34 @@ require 'thor'
 module Mmcli
  module Cli
    class Application < Thor
+     include Thor::Actions
 
-     @@manifest_list = []
-
-     desc 'mmcli MANIFEST [options] FILE(optional)', 'Add or create the manifest file MANIFEST and add or delete the file FILE.'
+     desc 'mmcli MANIFEST [options] FILE(optional)', 'Add file to, or create, the manifest file MANIFEST and add or delete the file FILE.'
      option :l
      option :d
      option :a
+     option :h
      def mmcli(manifest, file = nil)
+       if File.exist?(manifest)
+         f = File.open(manifest, "r+")
+       else
+         f = File.new(manifest, "w")
+       end
        if options[:d]
-         if @@manifest_list.include?(file)
-           @@manifest_list.delete(file)
+         if f.include?(file)
+           f.delete(file)
            puts "Successfully deleted #{file} from manifest."
          end
        elsif options[:a]
-         @@manifest_list << file
-         puts "Successfully added file to manifest."
+         if File.exist?(file)
+           f.write(file)
+           puts "Files successfully added to manifest."
+         else
+           puts "Error: could not find specified files."
+         end
        end
-       @output = ''
-       @@manifest_list.each do |item|
-         @output += "#{item} + \n"
-       end
-       puts "#{@output}" if options[:l]
+       puts "#{f}" if options[:l]
+       puts "-l to list the files in the manifest \n -a to add a file or files to the manifest \n -d to delete a file or files from the manifest \n -h for help" if options[:h]
      end
    end
  end
