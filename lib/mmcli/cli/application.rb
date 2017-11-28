cada93ee5587f76bf43115cdfe63380cf8766e99
ChristopherJamesN <nady.christopher@gmail.com>
2017-11-28 13:23:43 -0600

    Update options for command

--- lib/mmcli/cli/application.rb
+++ lib/mmcli/cli/application.rb
@@ -4,9 +4,27 @@ module Mmcli
  module Cli
    class Application < Thor
 
-     desc 'hello NAME', 'Display greeting with given NAME'
-     def hello(name)
-       puts "Hello #{name}"
+     @@manifest_list = []
+
+     desc 'mmcli MANIFEST [options] FILE(optional)', 'Add or create the manifest file MANIFEST and add or delete the file FILE.'
+     option :l
+     option :d
+     option :a
+     def mmcli(manifest, file = nil)
+       if options[:d]
+         if @@manifest_list.include?(file)
+           @@manifest_list.delete(file)
+           puts "Successfully deleted #{file} from manifest."
+         end
+       elsif options[:a]
+         @@manifest_list << file
+         puts "Successfully added file to manifest."
+       end
+       @output = ''
+       @@manifest_list.each do |item|
+         @output += "#{item} + \n"
+       end
+       puts "#{@output}" if options[:l]
      end
    end
  end
