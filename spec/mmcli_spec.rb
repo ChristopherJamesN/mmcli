cbdba6597a4e7042be92de135667cebc51cc09a5
ChristopherJamesN <nady.christopher@gmail.com>
2017-11-28 11:29:05 -0600

    Generate gem directory strucutre with bundler.

---
+++ spec/mmcli_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Mmcli do
+  it "has a version number" do
+    expect(Mmcli::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
