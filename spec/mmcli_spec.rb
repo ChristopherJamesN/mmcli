9a05b2a1941b1e6c6599cd452b4f6ef1fb8b6458
ChristopherJamesN <nady.christopher@gmail.com>
2017-11-29 13:00:57 -0600

    Update tests with correct parameters.

--- spec/mmcli_spec.rb
+++ spec/mmcli_spec.rb
@@ -1,61 +1,64 @@
 RSpec.describe Mmcli do
 
+  #Before run delete manifest
+  File.delete("manifest")
+
   it "has a version number" do
     expect(Mmcli::VERSION).not_to be nil
   end
 
-  describe ".mmcli" do
+  describe "Mmcli" do
 
     it "Can create and/or update the provided manifest file." do
-      args = ["mmcli", "-a", "b1.txt"]
-      options = Mmcli::Cli::Application.start(args)
-      expect(options).to eq({ "a" => true })
+      args = ["mmcli", "manifest", "-a", "b1.txt"]
+      Mmcli::Cli::Application.start(args)
+      expect(File.exist?("manifest")).to eq(true)
     end
 
     it "Each path in the manifest file should be on a separate line." do
-      args = ["mmcli", "-a", "c1.txt"]
-      options = Mmcli::Cli::Application.start(args)
-      expect(options).to eq({ "a" => true })
+      args = ["mmcli", "manifest", "-a", "c1.txt"]
+      Mmcli::Cli::Application.start(args)
+      expect(File.read("manifest")).to eq("b1.txt\nc1.txt\n")
     end
 
     it "Can list items in the manifest file in a case-insensitive alphabetical order." do
-      args =["mmcli", "-l"]
-      list = Mmcli::Cli::Application.start(args)
-      expect(list).to eq("b1.txt\nc1.txt")
+      args =["mmcli", "manifest", "-al", "b1.txt"]
+      Mmcli::Cli::Application.start(args)
+      expect(File.read("manifest")).to eq("b1.txt\nb1.txt\nc1.txt\n")
     end
 
     it "Specified path(s) should be added to the manifest file only if all the paths are files that actually exist." do
-      args =["mmcli", "-a", "d1.txt"]
-      list = Mmcli::Cli::Application.start(args)
-      expect(list).to eq("Error: could not find specified files.")
+      args =["mmcli", "manifest", "-a", "d1.txt"]
+      Mmcli::Cli::Application.start(args)
+      expect(File.read("manifest")).to eq("b1.txt\nb1.txt\nc1.txt\n")
     end
 
     it "The specified path(s) should be removed from the manifest file or ignored if they are not in the manifest." do
-      args =["mmcli", "-d", "b1.txt"]
-      list = Mmcli::Cli::Application.start(args)
-      expect(list).to eq("a1.txt")
+      args =["mmcli", "manifest", "-d", "b1.txt"]
+      Mmcli::Cli::Application.start(args)
+      expect(File.read("manifest")).to eq("c1.txt\n")
     end
 
     it "Does not accept the delete and add option at the same time." do
-      args =["mmcli", "-ad", "b1.txt"]
-      list = Mmcli::Cli::Application.start(args)
-      expect(list).to eq("You cannot specify both options add and delete simultaneously.")
+      args =["mmcli", "manifest", "-ad", "b1.txt"]
+      Mmcli::Cli::Application.start(args)
+      expect(File.read("manifest")).to eq("b1.txt\nc1.txt\n")
     end
 
     it "Can accept the list(-l) option with any other option, display after execution of other actions." do
-      args =["mmcli", "-al", "c1.txt"]
-      list = Mmcli::Cli::Application.start(args)
-      expect(list).to eq("b1.txt\nc1.txt")
+      args =["mmcli", "manifest", "-al", "c1.txt"]
+      Mmcli::Cli::Application.start(args)
+      expect(File.read("manifest")).to eq("c1.txt\nc1.txt\n")
     end
 
     it "Can print a success message if it executed successfully." do
-      args =["mmcli", "-al", "c1.txt"]
+      args =["mmcli", "manifest", "-al", "c1.txt"]
       list = Mmcli::Cli::Application.start(args)
       expect(list).to eq("Files successfully added to manifest.")
     end
 
     it "Can print a failure message if it failed to execute." do
-      args =["mmcli", "-al", "d1.txt"]
+      args =["mmcli", "manifest", "-al", "d1.txt"]
       list = Mmcli::Cli::Application.start(args)
       expect(list).to eq("Error: could not find specified files.")
     end
